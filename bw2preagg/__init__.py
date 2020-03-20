a0a6dc63b7550db3120065f2e363118ed15e0fb5
Pascal Lesage <pascal.lesage@polymtl.ca>
2020-03-19 21:31:30 -0400

    Add functions to __init__ for * import

--- bw2preagg/__init__.py
+++ bw2preagg/__init__.py
@@ -1,5 +1,21 @@
 __all__ = [
     'setup_project',
+    'generate_base_presamples',
+    'generate_balancing_presamples',
+    'dispatch_lci_calculators',
+    'set_up_lci_calculations',
+    'calculate_all_det_LCIA_score',
+    'save_all_LCIA_score_arrays',
+    'load_LCI_array',
+    'load_LCIA_array',
 ]
 
 from .setup_project import setup_project
+from .generate_base_presample_resource import generate_base_presamples
+from .generate_balancing_presample_resources import generate_balancing_presamples
+from .generate_LCI_samples import dispatch_lci_calculators, set_up_lci_calculations
+from .generate_lcia import calculate_all_det_LCIA_score, save_all_LCIA_score_arrays
+from .utils import load_LCI_array, load_LCIA_array
+
+# Need load det_LCI, load_det_LCIA
+# Harmonise calculate_all_det and save_all_LCIA --> generate, as elsewhere
