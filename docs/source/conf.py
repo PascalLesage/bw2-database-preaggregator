833c655c487d1fa9413d5e6eac6e7cc647132c47
Pascal Lesage <pascal.lesage@polymtl.ca>
2020-03-26 20:59:31 -0400

    Tweak doc conf file for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../bw2preagg/'))
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
